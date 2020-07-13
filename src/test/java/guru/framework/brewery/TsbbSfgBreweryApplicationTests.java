770936f82a1f36c212ba14ced7f7aa3a553f14ec
Kamal Eley <kamal.eley@eleysoftwareconsulting.com>
2020-07-13 17:25:56 -0400

    Initial commit.

---
+++ src/test/java/guru/framework/brewery/TsbbSfgBreweryApplicationTests.java
@@ -0,0 +1,15 @@
+package guru.framework.brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TsbbSfgBreweryApplicationTests
+{
+
+    @Test
+    public void contextLoads()
+    {
+    }
+
+}
